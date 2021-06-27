7fec88febf3cfd1760445ebb40df473830d65cfe
fhfuih <fhfuih@outlook.com>
2021-06-27 15:39:22 +0800

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# CQA 奥里给
+
+我暂时没有加jQuery，因为现在好多地方都不用jQuery了，纯原生API已经做的比较全（且比jQ快了）。
+如果想加jQuery的话，我在index.html里面留了一个comment直接打开就有了
---
+++ main.js
@@ -0,0 +1,115 @@
+function getAnswers(question) {
+  const mock = {
+    question: 'What is Lorem Ipsum?',
+    description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+    answers: [
+      {
+        content: `<p>Diam quam <strong>nulla</strong> porttitor massa id neque aliquam. Diam maecenas sed enim ut sem viverra aliquet. Nulla facilisi etiam dignissim diam quis.</p>
+<p>Parturient montes <em>nascetur</em> ridiculus mus mauris vitae ultricies. Facilisi cras fermentum odio eu. Gravida arcu ac tortor dignissim convallis aenean. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare.</p>
+<p>Et molestie ac feugiat sed lectus vestibulum mattis <em>ullamcorper</em>. Amet risus nullam eget felis eget nunc. Porta lorem mollis aliquam ut porttitor leo a diam sollicitudin. Ac orci phasellus egestas tellus rutrum.</p>`,
+        propositions: [
+          {
+            content: 'Diam quam nulla porttitor massa id neque aliquam',
+            category: 'lorem',
+          },
+          {
+            content: 'Parturient montes nascetur ridiculus mus mauris vitae ultricies',
+            category: 'foo',
+          },
+          {
+            content: 'Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper',
+            category: 'bar',
+          }
+        ]
+      },
+      {
+        content: `<p>Leo vel fringilla est ullamcorper eget nulla.</p>
+<p>Mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing.</p>`,
+        propositions: [
+          {
+            content: 'Leo vel fringilla est ullamcorper eget nulla',
+            category: 'foo',
+          },
+          {
+            content: 'Mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing',
+            category: 'bar',
+          }
+        ]
+      }
+    ]
+  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(mock)
+    }, 1000)
+  })
+}
+
+// 等价于jQuery的 $.ready(...) 即 $(...)
+document.addEventListener('DOMContentLoaded', async () => {
+  const res = await getAnswers();
+  console.log(res)
+  const {question, description, answers} = res;
+
+  // 加载问题
+  document.getElementById('question').textContent = question
+  document.getElementById('question-description').textContent = description
+
+  // 加载所有回答
+  const answerContainer = document.getElementById('answer-container')
+  const answerTemplate = document.getElementById('template-answer').content.firstElementChild
+  answers.forEach((ans, ansIdx) => {
+    // 加载单个回答的内容
+    const answerNode = answerTemplate.cloneNode(true)
+    answerNode.classList.add(`answer-${ansIdx}`)
+    answerNode.innerHTML = ans.content
+    answerContainer.append(answerNode)
+
+    // mark单个回答的所有proposition
+    const markContext = new Mark(answerNode)
+    ans.propositions.forEach(({content, category}, propIdx) => {
+      // mark单个回答的单个proposition
+      markContext.mark(content, {
+        className: `proposition proposition-${propIdx}`,
+        separateWordSearch: false,
+        acrossElements: true,
+      })
+
+      // 给这个proposition加checkbox
+      const markedElements = document.querySelectorAll(`.answer-${ansIdx} .proposition-${propIdx}`)
+      const markedLast = markedElements[markedElements.length - 1]
+      const checkbox = document.createElement('input')
+      checkbox.type = 'checkbox'
+      checkbox.classList.add('form-check-input')
+      markedLast.insertAdjacentElement('afterend', checkbox)
+      // 点击proposition的时候自动check
+      const noteContainer = document.getElementById('note-container')
+      markedElements.forEach(el => {
+        el.addEventListener('click', () => {
+          if (!checkbox.checked) { // 如果没有check，check并加入note
+            checkbox.checked = true
+            const noteElement = document.getElementById('template-note').content.firstElementChild.cloneNode(true)
+            noteElement.textContent = answers[ansIdx].propositions[propIdx].content
+            noteElement.setAttribute('data-answer', ansIdx)
+            noteElement.setAttribute('data-proposition', propIdx)
+            noteContainer.append(noteElement)
+            noteContainer.nextElementSibling.classList.add('d-none')
+          } else { // uncheck并移除
+            checkbox.checked = false
+            const noteElement = noteContainer.querySelector(`[data-answer="${ansIdx}"][data-proposition="${propIdx}"]`)
+            noteElement.remove()
+            if (noteContainer.childElementCount == 1) noteContainer.nextElementSibling.classList.remove('d-none')
+          }
+        })
+        el.addEventListener('mouseenter', () => {
+          checkbox.style.visibility = 'visible'
+        })
+        el.addEventListener('mouseleave', () => {
+          checkbox.style.removeProperty('visibility')
+        })
+      })
+    })
+
+  })
+
+})
